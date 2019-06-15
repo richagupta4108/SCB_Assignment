80d1325f3bcdd89087370ed84cff7d80a963f434
richagupta4108 <richagupta4108@gmail.com>
2019-06-15 21:04:20 +0530

    changes for can activate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,15 +6,17 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ExploreComponent } from './explore/explore.component';
 import { SettingsComponent } from './settings/settings.component';
 import { DetailsComponent } from './details/details.component';
+import { AuthGuard } from './services/auth-gaurd.service';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'explore', component: ExploreComponent },
-  { path: 'settings', component: SettingsComponent },
-  { path: 'details/:id', component: DetailsComponent},
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent , canActivate: [AuthGuard]},
+  { path: 'explore', component: ExploreComponent , canActivate: [AuthGuard]},
+  { path: 'settings', component: SettingsComponent, canActivate: [AuthGuard] },
+  { path: 'details/:id', component: DetailsComponent , canActivate: [AuthGuard]},
+  { path: '', redirectTo: '/login', pathMatch: 'full', canActivate: [AuthGuard]},
+  { path: '**', redirectTo: '/login', pathMatch: 'full', canActivate: [AuthGuard]},
 ];
 
 @NgModule({
